e63a2142c5373e16b3bfd6218dc1447da0b7a937
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2014-04-22 11:10:57 +0300

    salam

---
+++ src/main/java/com/arvue/apps/jadid/Main.java
@@ -0,0 +1,11 @@
+package com.arvue.apps.jadid;
+
+import org.vaadin.mideaas.MideaasComponent;
+
+public class Main extends MideaasComponent {
+
+    @Override
+    public void attach() {
+        super.attach();
+    }
+}
